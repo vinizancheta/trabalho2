91c5c41bd5d0a590f3bad14be60794e3fc011b89
Vinicius Zancheta <vinicius.zancheta@unesp.br>
2024-10-08 20:05:52 -0300

    Primeiro commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const userFavoritesUrl =
+      'https://shop-cod3r-d4e38-default-rtdb.firebaseio.com/';
+  static const productBaseUrl =
+      'https://shop-cod3r-d4e38-default-rtdb.firebaseio.com/';
+  static const orderBaseUrl =
+      'https://shop-cod3r-d4e38-default-rtdb.firebaseio.com/';
+  static const webApiKey = 'AIzaSyAOB9uer_xxRprUZnoZSqeK3oenNmingOc';
+}
